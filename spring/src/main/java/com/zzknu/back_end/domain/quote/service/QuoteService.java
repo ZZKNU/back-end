bec69559730b5d5e6439df85b5ed8d7c481f0efe
milk-stone <ywshero@naver.com>
2024-07-29 00:03:17 +0900

    feat(QuoteService.java): service Annotation 추가함
    앞서 저질렀던 PR 실패를 복구하기위한 노력

--- spring/src/main/java/com/zzknu/back_end/domain/quote/service/QuoteService.java
+++ spring/src/main/java/com/zzknu/back_end/domain/quote/service/QuoteService.java
@@ -1,4 +1,9 @@
 package com.zzknu.back_end.domain.quote.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class QuoteService {
 }
