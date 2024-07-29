73adf9efab0f577cb0f4aac31b2864756628241a
young-in <hardworkingini@gmail.com>
2024-07-30 02:59:10 +0900

    feat(User) : User 데이터 설정을 위한 Setter 어노테이션 추가

--- spring/src/main/java/com/zzknu/back_end/domain/user/entity/User.java
+++ spring/src/main/java/com/zzknu/back_end/domain/user/entity/User.java
@@ -8,12 +8,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Setter
 public class User extends BaseEntityWithUpdatedAt {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
