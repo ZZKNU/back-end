a07c1bde762e7e2a0980b02b3423e0352d379490
young-in <hardworkingini@gmail.com>
2024-07-30 03:00:40 +0900

    feat(UserController) : User 정보를 확인/수정하고 User을 삭제하는 api 작성

--- spring/src/main/java/com/zzknu/back_end/domain/user/controller/UserController.java
+++ spring/src/main/java/com/zzknu/back_end/domain/user/controller/UserController.java
@@ -1,11 +1,36 @@
 package com.zzknu.back_end.domain.user.controller;
 
+import com.zzknu.back_end.domain.user.dto.UserUpdateDto;
+import com.zzknu.back_end.domain.user.entity.User;
 import com.zzknu.back_end.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    // 개인 정보 확인 (GET)
+    @GetMapping
+    public ResponseEntity<User> getUserInfo(@RequestParam Long userId) {
+        User user = userService.getUserById(userId);
+        return ResponseEntity.ok(user);
+    }
+
+    // 개인 정보 수정 (PUT)
+    @PutMapping
+    public ResponseEntity<User> updateUserInfo(@RequestParam Long userId, @RequestBody UserUpdateDto updatedUser) {
+        User user = userService.updateUser(userId, updatedUser);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUserInfo(@RequestParam Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
