4daa66f16d596ef0ff4fae4eb55adb527acd3259
young-in <hardworkingini@gmail.com>
2024-07-30 03:00:23 +0900

    feat(UserService) : User 정보를 확인/수정하고 User을 삭제하는 service code 작성

--- spring/src/main/java/com/zzknu/back_end/domain/user/service/UserService.java
+++ spring/src/main/java/com/zzknu/back_end/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.zzknu.back_end.domain.user.service;
 
+import com.zzknu.back_end.domain.user.dto.UserUpdateDto;
+import com.zzknu.back_end.domain.user.entity.User;
 import com.zzknu.back_end.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    // 사용자 정보 조회
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    // 사용자 정보 수정
+    public User updateUser(Long userId, UserUpdateDto updatedUser) {
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        updatedUser.update(existingUser);
+
+        return userRepository.save(existingUser);
+    }
+
+    // 회원 탈퇴
+    public void deleteUser(Long userId) {
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.delete(existingUser);
+    }
 }
