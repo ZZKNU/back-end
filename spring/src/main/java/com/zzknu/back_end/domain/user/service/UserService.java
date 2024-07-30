61d20276c67df6304252368fd90ad827d557e879
young-in <hardworkingini@gmail.com>
2024-07-31 02:08:23 +0900

    feat(UserService) : 이름으로 사용자 검색 / 아이디로 사용자 검색하는 method 구현

--- spring/src/main/java/com/zzknu/back_end/domain/user/service/UserService.java
+++ spring/src/main/java/com/zzknu/back_end/domain/user/service/UserService.java
@@ -1,9 +1,12 @@
 package com.zzknu.back_end.domain.user.service;
 
+import com.zzknu.back_end.domain.friendship.dto.FriendInfoDto;
 import com.zzknu.back_end.domain.user.dto.UserUpdateDto;
 import com.zzknu.back_end.domain.user.entity.User;
 import com.zzknu.back_end.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +16,12 @@ public class UserService {
 
     // 사용자 정보 조회
     public User getUserById(Long userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        return findById(userId);
     }
 
     // 사용자 정보 수정
     public User updateUser(Long userId, UserUpdateDto updatedUser) {
-        User existingUser = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
+        User existingUser = findById(userId);
         updatedUser.update(existingUser);
 
         return userRepository.save(existingUser);
@@ -28,8 +29,20 @@ public class UserService {
 
     // 회원 탈퇴
     public void deleteUser(Long userId) {
-        User existingUser = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        User existingUser = findById(userId);
         userRepository.delete(existingUser);
+    }
+
+    // 이름으로 사용자 검색
+    public Page<FriendInfoDto> findUsersByName(String name, Pageable pageable) {
+        Page<User> users =  userRepository.findByNickName(name, pageable);
+        Page<FriendInfoDto> friendInfoDtos = users.map(FriendInfoDto::new);
+        return friendInfoDtos;
+    }
+
+    // Id로 사용자 검색
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
