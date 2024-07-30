7f611b8880492f76789eaefd32c3d64a18482a8c
young-in <hardworkingini@gmail.com>
2024-07-31 02:13:10 +0900

    feat(FriendRepository) : 팔로잉/팔로워 목록가져오기 위한 method 구현

--- spring/src/main/java/com/zzknu/back_end/domain/friendship/repository/FriendshipRepository.java
+++ spring/src/main/java/com/zzknu/back_end/domain/friendship/repository/FriendshipRepository.java
@@ -2,8 +2,17 @@ package com.zzknu.back_end.domain.friendship.repository;
 
 
 import com.zzknu.back_end.domain.friendship.entity.Friendship;
+import com.zzknu.back_end.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
+    @Query("SELECT f.to_user FROM Friendship f WHERE f.from_user.id = :userId")
+    Page<User> findFollowingByUserId(Long userId, Pageable pageable);
+
+    @Query("SELECT f.from_user FROM Friendship f WHERE f.to_user.id = :userId")
+    Page<User> findFollowersByUserId(Long userId, Pageable pageable);
 }
