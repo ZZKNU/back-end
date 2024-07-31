0081bd0fad54eb44f58f91a1817368251fc7d2d8
young-in <hardworkingini@gmail.com>
2024-07-31 21:02:10 +0900

    refactor(User) : cors 에러 수정

---
+++ spring/src/main/java/com/zzknu/back_end/security/WebConfig.java
@@ -0,0 +1,19 @@
+package com.zzknu.back_end.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:5173") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 쿠키 전송 허용 여부
+    }
+}
+
