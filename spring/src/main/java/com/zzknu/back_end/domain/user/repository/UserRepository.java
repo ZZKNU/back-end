2e3c7fdac4645da7553a5e0c8f17b24650e6eeaf
young-in <hardworkingini@gmail.com>
2024-08-05 21:48:20 +0900

    feat(userRepository) : 사용자 닉네임으로 검색하는 method 수정

--- spring/src/main/java/com/zzknu/back_end/domain/user/repository/UserRepository.java
+++ spring/src/main/java/com/zzknu/back_end/domain/user/repository/UserRepository.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Page<User> findByNickname(String nickname, Pageable pageable);
     Optional<User> findByEmailAndPassword(String email, String password);
     Optional<User> findByEmail(String email);
     Optional<User> findByNickname(String nickname);
+    Page<User> findByNicknameContaining(String name, Pageable pageable);
 }
